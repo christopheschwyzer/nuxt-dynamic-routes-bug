6d7a5e2c0597efd1e5b66a1d673481b2255ac77b
kinchiller <kingchiller@gmail.com>
2018-12-31 09:49:11 +0100

    Init

---
+++ pages/foo/_bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt-link :to="{params: {bar: 1}}">Bar 1</nuxt-link>
+    <nuxt-link :to="{params: {bar: 2}}">Bar 2</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log("created");
+  }
+};
+</script>
+
+<style>
+</style>
